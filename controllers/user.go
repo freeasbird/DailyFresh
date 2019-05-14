27874f706c41b6384106fe2196f6df5414b5d6b7
zhoushaodong <1138894663@qq.com>
2019-05-14 23:00:38 +0800

    提交注册请求

--- controllers/user.go
+++ controllers/user.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"regexp"
+)
 
 type UserController struct {
 	beego.Controller
@@ -11,5 +14,14 @@ func (this *UserController) ShowReg() {
 }
 
 func (this *UserController) HandleReg() {
+	username := this.GetString("username")
+	pwd := this.GetString("pwd")
+	cpwd := this.GetString("cpwd")
+	email := this.GetString("email")
+	if username == "" || pwd == "" || cpwd == "" || email == "" {
+		this.Data["errmssg"] = "填写数据不完整"
+		this.TplName = "home/user/register.html"
+	}
+	regexp.Compile("")
 
 }
